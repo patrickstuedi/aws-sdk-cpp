e6699ba063fac49e394b4f89beff2d2620371509
Patrick Stuedi <pstuedi@confluent.io>
2021-07-06 22:46:58 +0200

    ubuntu deps

--- sdk_build/build.sh
+++ sdk_build/build.sh
@@ -1,2 +1,3 @@
+sudo apt-get install libcurl4-openssl-dev libssl-dev uuid-dev zlib1g-dev libpulse-dev
 cmake .. -DCMAKE_BUILD_TYPE=Debug -DENABLE_TESTING=OFF
 cmake .. -DCMAKE_BUILD_TYPE=Debug -DENABLE_TESTING=OFF -DNO_ENCRYPTION=ON -DBUILD_ONLY="s3;kinesis;transfer"
